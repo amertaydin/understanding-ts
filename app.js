542d0b98a2c3a2b81ba5d745012b036c0b10fd0c
amertaydin <amertaydin14@gmail.com>
2024-08-02 14:45:25 +0200

    add types to parameters

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Your code goes here!");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = "5";
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
