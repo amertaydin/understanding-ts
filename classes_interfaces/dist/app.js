3c60b57759a6f0b14dc03370380841f8e9c38f3c
amertaydin <amertaydin14@gmail.com>
2024-08-07 00:39:30 +0200

    implement oop principles

--- classes_interfaces/dist/app.js
+++ classes_interfaces/dist/app.js
@@ -5,8 +5,8 @@ class Department {
         this.name = name;
         this.employees = [];
     }
-    describe() {
-        console.log(`Department ${this.id}: ${this.name}`);
+    static createEmployee(name) {
+        return { name: name };
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -16,10 +16,74 @@ class Department {
         console.log(this.employees);
     }
 }
-const software = new Department("d1", "Software");
+Department.fiscalYear = 2024;
+class SoftwareDepartment extends Department {
+    describe() {
+        console.log(`Software Department - ID ${this.id}`);
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "Accounting");
+        this.admins = admins;
+    }
+    describe() {
+        console.log(`Accounting Department - ID ${this.id}`);
+    }
+}
+class FinanceDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Finance");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    describe() {
+        console.log(`Finance Department - ID ${this.id}`);
+    }
+    addEmployee(employee) {
+        if (employee == "Frank") {
+            return;
+        }
+        this.employees.push(employee);
+    }
+    addReport(report) {
+        this.reports.push(report);
+        this.lastReport = report;
+    }
+    get mostRecentReport() {
+        if (this.lastReport)
+            return this.lastReport;
+        throw new Error("No report found");
+    }
+    set mostRecentReport(report) {
+        if (!report) {
+            throw new Error("Please provide a valid value");
+        }
+        this.addReport(report);
+    }
+}
+const employee1 = Department.createEmployee("Nicole");
+console.log(employee1, Department.fiscalYear);
+const software = new SoftwareDepartment("d1", "Software");
 software.addEmployee("Mert");
 software.addEmployee("Batur");
 software.addEmployee("Ali");
 software.describe();
 software.printEmployeeInformation();
+const accounting = new AccountingDepartment("d2", ["Jenny", "John"]);
+accounting.addEmployee("Lebron");
+accounting.addEmployee("Kobe");
+accounting.addEmployee("Dwight");
+accounting.describe();
+accounting.printEmployeeInformation();
+console.log(accounting);
+const finance = new FinanceDepartment("d3", ["market", "btc"]);
+finance.addReport("food");
+console.log(finance.mostRecentReport);
+finance.mostRecentReport = "prices";
+console.log(finance.mostRecentReport);
+finance.addEmployee("Kimiko");
+finance.addEmployee("Frank");
+finance.describe();
+console.log(finance);
 //# sourceMappingURL=app.js.map
--- classes_interfaces/src/app.ts
+++ classes_interfaces/src/app.ts
@@ -1,10 +1,19 @@
-class Department {
-  private employees: string[] = [];
+abstract class Department {
+  static fiscalYear = 2024;
+  // private readonly id : string;
+  // public name: string;
+  protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {}
+  constructor(protected readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = name;
+  }
 
-  describe() {
-    console.log(`Department ${this.id}: ${this.name}`);
+  // No implementation since it's abstract
+  abstract describe(): void;
+
+  static createEmployee(name: string) {
+    return { name: name };
   }
 
   addEmployee(employee: string) {
@@ -17,7 +26,67 @@ class Department {
   }
 }
 
-const software = new Department("d1", "Software");
+class SoftwareDepartment extends Department {
+  describe(): void {
+    console.log(`Software Department - ID ${this.id}`);
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "Accounting");
+  }
+
+  describe(): void {
+    console.log(`Accounting Department - ID ${this.id}`);
+  }
+}
+
+class FinanceDepartment extends Department {
+  private lastReport: string;
+  private reports: string[];
+  constructor(id: string, reports: string[]) {
+    super(id, "Finance");
+    this.reports = reports;
+    this.lastReport = reports[0];
+  }
+
+  describe(): void {
+    console.log(`Finance Department - ID ${this.id}`);
+  }
+
+  // Overriding the addEmployee method in base class
+  addEmployee(employee: string): void {
+    if (employee == "Frank") {
+      return;
+    }
+    this.employees.push(employee);
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+    this.lastReport = report;
+  }
+
+  // getter
+  get mostRecentReport(): string {
+    if (this.lastReport) return this.lastReport;
+    throw new Error("No report found");
+  }
+
+  // settter
+  set mostRecentReport(report: string) {
+    if (!report) {
+      throw new Error("Please provide a valid value");
+    }
+    this.addReport(report);
+  }
+}
+
+const employee1 = Department.createEmployee("Nicole");
+console.log(employee1, Department.fiscalYear);
+
+const software = new SoftwareDepartment("d1", "Software");
 software.addEmployee("Mert");
 software.addEmployee("Batur");
 software.addEmployee("Ali");
@@ -28,3 +97,21 @@ software.printEmployeeInformation();
 // Since this doesn't have name propert it will print undefined
 // const softwareCopy1 = { describe: software.describe };
 // softwareCopy1.describe();
+
+const accounting = new AccountingDepartment("d2", ["Jenny", "John"]);
+accounting.addEmployee("Lebron");
+accounting.addEmployee("Kobe");
+accounting.addEmployee("Dwight");
+accounting.describe();
+accounting.printEmployeeInformation();
+console.log(accounting);
+
+const finance = new FinanceDepartment("d3", ["market", "btc"]);
+finance.addReport("food");
+console.log(finance.mostRecentReport);
+finance.mostRecentReport = "prices";
+console.log(finance.mostRecentReport);
+finance.addEmployee("Kimiko");
+finance.addEmployee("Frank");
+finance.describe();
+console.log(finance);
