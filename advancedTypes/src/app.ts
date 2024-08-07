fb861cc13852dba9918e0a05089406811c6833d2
amertaydin <amertaydin14@gmail.com>
2024-08-07 20:48:19 +0200

    function overloading

--- advancedTypes/src/app.ts
+++ advancedTypes/src/app.ts
@@ -34,6 +34,9 @@ type Universal = Combinable & Numeric; // this is the intersection for types so 
 
 // interface ElevatedEmployee extends Admin, Employee {}
 
+// this overloads the add funciton below
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
